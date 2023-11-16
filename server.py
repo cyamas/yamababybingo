e16cd238787f0da790bca822da0e2190dd9a1378
cyamas <chaseyyamashiro@gmail.com>
2023-11-15 22:29:09 -0800

    Large commit. Changed many many things.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import join_room, leave_room, send, SocketIO, emit
+from flask_redis import FlaskRedis
 import random
 import os
 from datetime import timedelta
@@ -10,14 +11,17 @@ import threading
 
 app = Flask(__name__)
 
-app.secret_key = 'ybaambay012193'
+app.secret_key = 'ybaambay012194'
+
+session
+
+app.config['REDIS_URL'] = 'redis://localhost:6379/0'
 
 socketio = SocketIO(app, cors_allowed_origins="*")
+redis = FlaskRedis(app)
 
-app.permanent_session_lifetime = timedelta(minutes=90)
 
 rooms = []
-
 
 @app.route('/')
 def home():
@@ -32,69 +36,125 @@ def action():
     else:
         return join_game(data)
 
+@app.route('/host', methods=['GET', 'POST'])
+def host_game():
+    bingo_items = []
+    new_curr_name = ''
+    new_prev_name = ''
+    with open('static/bingoItems.json') as itms:
+        bingo_items += json.load(itms)
+    curr_i = int(redis.get('item_index').decode('utf-8'))
+    item_indices = json.loads(redis.get('item_order'))
+    item_indices = [int(item) for item in item_indices]
+    
+    if curr_i >= 1:
+        new_curr_name = bingo_items[item_indices[curr_i-1]]['name']
+        if curr_i > 1:
+            new_prev_name = bingo_items[item_indices[curr_i-2]]['name']
+    
+    if request.method == 'POST':
+        new_curr_name = bingo_items[item_indices[curr_i]]['name']
+        if curr_i > 0: 
+            new_prev_name = bingo_items[item_indices[curr_i-1]]['name']
+        
+        new_curr = bingo_items[item_indices[curr_i]]['img']
+        new_prev = redis.get('curr').decode('utf-8') 
+        redis.set('prev', new_prev)
+        redis.set('curr', new_curr)
+        redis.set('item_index', curr_i + 1)
+        redis.rpush('item_history', new_curr)
+        socketio.emit('items', [new_curr, new_prev]) 
+
+    curr_item = redis.get('curr').decode('utf-8')
+    prev_item = redis.get('prev').decode('utf-8')
+
+    return render_template('/host.html', curr = curr_item, prev = prev_item, curr_name = new_curr_name, prev_name = new_prev_name)
+
+@app.route('/bingo/<team_name>', methods = ['GET','POST'])
+def play(team_name):
+    team = request.path[7:]
+    curr_xes = []
+    if request.method == 'POST':
+        data = request.form
+        team = data['team']
+        curr_xes = json.loads(redis.get(data['team'] + '_xes'))
+        curr_xes[int(data['id'])] = int(data['x'])
+        new_xes = json.dumps(curr_xes)
+        redis.set(data['team'] + '_xes', new_xes)
+        
+    socketio.emit('currXes', curr_xes)  
+    bingo_card = json.loads(redis.get(team + '_card'))
+    xes_list = json.loads(redis.get(team + '_xes'))
+    xes_list = [int(x) for x in xes_list]
+    curr_item = redis.get('curr').decode('utf-8')
+    prev_item = redis.get('prev').decode('utf-8')
+
+    return render_template('/bingo.html', card = bingo_card, xes = xes_list, team_name = team_name, curr = curr_item, prev = prev_item)
+
 #creates session data to store real-time game information for 90 minutes
 def create_game(password):
-    session.permanent = True
     rooms.append(password)
+    print("Rooms: ", rooms)
+    redis.lpush('players', 'host')
+    redis.set('item_index', 0)
+    redis.set('curr', 'None')
+    redis.set('prev', 'None')
 
-    session['password'] = password
-    session['host'] = True
-    session['curr'] = -1
-    session['prev'] = -2
     registry_length = len(os.listdir('static/gameImages'))-2
-    session['game_index'] = random.sample(range(0,registry_length-1), registry_length-1)
-    session['game_i'] = -2
-    session['started'] = False
+    game_item_list = json.dumps(random.sample(range(0,registry_length-1), registry_length-1))
+    redis.set('item_order', game_item_list)
     return redirect(url_for('host_game'))
 
 #gets password from client and allows a user entrance into the game
 def join_game(data):
-    if data['password'] in rooms:
-        session['idx'] = make_bingo_card()
-        session['team'] = data['team-name']
-        session['xes'] = [0 for i in range(25)]
+    team_list = redis.lrange('players', 0, -1) 
+    if data['password'] in rooms and data['team-name'] not in team_list:
+        session['password'] = data['password']
+        team = data['team-name']
+        card =json.dumps(make_bingo_card())
+        print(card)
+        xes = json.dumps([0 for i in range(25)])
+        redis.rpush('players', team)
+        redis.set(team + '_card', card)
+        redis.set(team + '_xes', xes)
+    
         return redirect(url_for('play', team_name = data['team-name']))
     else:
-        return render_template('/home.html', error="Incorrect password. Try again.")
+        return render_template('/home.html', error="Password incorrect OR team name already taken!")
 
 #generates a random list of integers that will populate the bingo card with images based on their index position
 def make_bingo_card():
-    registry_length = len(os.listdir('static/gameImages'))
-    idx = random.sample(range(0,registry_length-1), 25)
-    return idx
-
-@app.route('/bingo/<team_name>', methods = ['GET','POST'])
-def play(team_name):
-    if request.method == 'POST':
-        data = request.form
-        session['xes'][int(data['id'])] = int(data['x'])
-
-    return render_template('/bingo.html', idx = session['idx'], xes = session['xes'], team_name = team_name)
-
-
-@socketio.on("connect")
-def connect(auth):
-    print('Someone has joined the game!')
-    
-    return
-
-@app.route('/host', methods=['GET', 'POST'])
-def host_game():
     bingo_items = []
     with open('static/bingoItems.json') as itms:
         bingo_items += json.load(itms)
-    if request.method == 'POST':
-        print([session['prev'], session['curr'], session['game_i']])
-        session['started'] = True
-        if session['game_i'] < len(session['game_index']) and session['started']:
-            new_prev = session['curr']
-            session['prev'] = new_prev
-            game_index = session['game_index']
-            curr_i = session['game_i']
-            session['curr'] = game_index[curr_i]
-            session['game_i'] += 1
-        socketio.emit('items', [bingo_items[session['curr']]['img'], bingo_items[session['prev']]['img']])   
-    return render_template('/host.html', curr = bingo_items[session['curr']], prev = bingo_items[session['prev']])
+    registry_length = len(os.listdir('static/gameImages'))
+    idx = random.sample(range(0,registry_length-1), 25)
+    card_pics = [bingo_items[indx]['img'] for indx in idx]
+    return card_pics
+
+@socketio.on("connect")
+def handle_connect(auth):
+    return
+
+@socketio.on('team')
+def team_name(team):
+    print(f"{team['team']} has joined the game!")
+    return
+
+@socketio.on("winner")
+def winner(data):
+    team = data['team']
+    candidate = data['candidate']
+    history = redis.lrange('item_history', 0, -1)
+    history = [item.decode('utf-8') for item in history]
+    if set(candidate).issubset(history):
+        socketio.emit("winningTeam", {'message': f" BINGOOOOOO!!! TEAM {team} HAS WON!!"})
+        print(f"BINGOOOOOO!!!!!!! TEAM {team} HAS WON!!")
+    else:
+        socketio.emit("invalid", team)
+        print(f"{team} has made an error! NO BINGO!!")
+    return
+
             
 if __name__ == '__main__':
     app.run(debug=True)
--- static/app.js
+++ static/app.js
@@ -1,241 +1,37 @@
-const rows = 5;
-const cols = 5;
+//Creates socket global variable without connecting
+var socket = io({autoConnect: false});
 
-const gameItems = [
-    {
-        "name": "INFANT LOUNGER",
-        "img": "static/gameImages/lounger.png",
-        "link": "https://snugglemeorganic.com/products/infant-lounger-gingerbread"
+var currXes = [];
+for (i=0; i < 25; i++) {currXes.push(0)}; 
 
-    },
-    {
-        "name": "HIGHLAND COO ONESIE",
-        "img": "static/gameImages/highlandCoo.png",
-        "link": "https://milkbarnkids.com/product/highland-cow-bamboo-short-sleeve-one-piece/"
-    },
-    {
-        "name": "BLACK AND TAN MOCCASIN",
-        "img": "static/gameImages/blackTanMocc.png",
-        "link": "https://birdrockbaby.com/collections/fringeless-moccasins/products/black-and-tan-fringeless-moccasins?variant=12536620056654"
-    },
-    {
-        "name": "UPPABABY MINU V2 STROLLER",
-        "img": "static/gameImages/stroller.png",
-        "link": "https://www.babylist.com/gp/uppababy-minu-v2-stroller/25173/1158319?is_group_gift=true&reg_item_id=472468524&registry_id=7596939"
-    },
-    {
-        "name": "FRIDABABY SOFT SINK BABY BATH",
-        "img": "static/gameImages/babyBath.png",
-        "link": "https://www.babylist.com/gp/fridababy-soft-sink-baby-bath/22930/923581?reg_item_id=472464751&registry_id=7596939"
-    },
-    {
-        "name": "LOULOU SILICONE BIB",
-        "img": "static/gameImages/bib.png",
-        "link": "https://www.amazon.com/dp/B087KVBYXG?psc=1&tag=babyli-20&th=1"
-    },
-    {
-        "name": "GRACO PACK N' PLAY PLAYARD",
-        "img": "static/gameImages/playard.png",
-        "link": "https://www.babylist.com/gp/graco-pack-n-play-portable-playard/15345/1111850?reg_item_id=473068041&registry_id=7596939"
-    },
-    {
-        "name": "STOKKE TRIP TRAP HIGH CHAIR",
-        "img": "static/gameImages/highchair.png",
-        "link": "https://www.babylist.com/gp/stokke-tripp-trapp-highchair/14772/455819?is_group_gift=true&reg_item_id=472021790&registry_id=7596939"
-    },
-    {
-        "name": "OMNI 360 BABY CARRIER",
-        "img": "static/gameImages/babyBackpack.png",
-        "link": "https://www.babylist.com/gp/ergobaby-omni-360-baby-carrier/13423/20735?reg_item_id=472015176&registry_id=7596939"
-    },
-    {
-        "name": "NUNA RAVA CONVERTIBLE CARSEAT",
-        "img": "static/gameImages/carseat.png",
-        "link": "https://www.babylist.com/gp/nuna-2019-rava-convertible-car-seat/14782/121425?is_group_gift=true&reg_item_id=472013357&registry_id=7596939"
-    },
-    {
-        "name": "LIFE AQUATIC BUCKET HAT",
-        "img": "static/gameImages/bucketHat.png",
-        "link": "https://pehr.com/collections/clothing/products/bucket-hat?variant=39853583892568"
-    },
-    {
-        "name": "AVANCHY FEEDING SET",
-        "img": "static/gameImages/feedingSet.png",
-        "link": "https://www.babylist.com/gp/avanchy-avanchy-x-babylist-bamboo-complete-feeding-gift-set/25649/1192317?reg_item_id=472015536&registry_id=7596939"
-    },
-    {
-        "name": "TWO-PIECE SNUGGLE SET",
-        "img": "static/gameImages/twoPieceJogger.png",
-        "link": "https://coloredorganics.com/products/organic-baby-2-piece-snuggle-set?variant=39757673824352"
-    },
-    {
-        "name": "DEKOR DIAPER PAIL IN SOFT MINT",
-        "img": "static/gameImages/diaperCan.png",
-        "link": "https://www.amazon.com/dp/B01JZD32PW?psc=1&tag=blvisitor-20&th=1"
-    },
-    {
-        "name": "BABYLETTO CHANGING TRAY",
-        "img": "static/gameImages/changingTray.png",
-        "link": "https://babyletto.com/collections/dressers/products/universal-removable-changing-tray?variant=8835241738294"
-    },
-    {
-        "name": "BABYLETTO CHANGING PAD",
-        "img": "static/gameImages/changingPad.png",
-        "link": "https://babyletto.com/collections/dressers/products/pure-31-inch-contour-changing-pad?variant=39412482408502"
-    },
-    {
-        "name": "BABYLETTO CHANGING COVER: TERRACOTTA",
-        "img": "static/gameImages/padCoverTerracotta.png",
-        "link": "https://babyletto.com/collections/changing-pad-covers/products/quilted-changing-pad-cover-in-gots-certified-organic-muslin-cotton?variant=40560021635126"
-    },
-    {
-        "name": "LOULOU TEETHER RATTLE",
-        "img": "static/gameImages/loulouTeether.png",
-        "link": "https://www.babylist.com/gp/loulou-lollipop-bubble-silicone-wood-teether/13757/30245?reg_item_id=472019202&registry_id=7596939"
-    },
-    {
-        "name": "JACK CARDIGAN",
-        "img": "static/gameImages/jackCardigan.png",
-        "link": "https://jamiekay.com/products/jack-cardigan-soft-lake-marle?variant=40370270961745",
-        "size": "130px"
-    },
-    {
-        "name": "GINGERSNAP MOCCASINS",
-        "img": "static/gameImages/gingersnapMoccs.png",
-        "link": "https://birdrockbaby.com/collections/fringeless-moccasins/products/gingersnap-fringeless-sherpa-moccasins?variant=43569872273628"
-    },
-    {
-        "name": "KIMONO OUTFIT BUNDLE",
-        "img": "static/gameImages/kimonoBundle.png",
-        "link": "https://coloredorganics.com/products/organic-cotton-baby-meadow-ribbed-outfit-set?variant=39632366731360"
-    },
-    {
-        "name": "JACK MODAL SLING",
-        "img": "static/gameImages/jackModalSling.png",
-        "link": "https://wildbird.co/products/jack-modal-sling?irclickid=W05QqQ3PUxyPRjuydqQVyUGPUkFTU4wpw0BXRE0&utm_source=impact&utm_medium=affiliate&utm_campaign=BabyList%20Inc&utm_content=65668&utm_term=&irgwc=1"
-    },
-    {
-        "name": "BABYLETTO CHANGING COVER: OAT STRIPE",
-        "img": "static/gameImages/padCoverOatStripe.png",
-        "link": "https://babyletto.com/collections/changing-pad-covers/products/quilted-changing-pad-cover-in-gots-certified-organic-muslin-cotton?variant=40560200876086"
-    },
-    {
-        "name": "SOLLY BABY WRAP CARRIER",
-        "img": "static/gameImages/wrapCarrier.png",
-        "link": "https://www.babylist.com/gp/solly-baby-wrap-carrier/14574/1163903?reg_item_id=472018793&registry_id=7596939"
-    },
-    {
-        "name": "BURT'S BEES BURP CLOTHS",
-        "img": "static/gameImages/burpCloths.png",
-        "link": "https://www.babylist.com/gp/burt-s-bees-baby-organic-burp-cloth-5-pack/14442/1268943?reg_item_id=465390662&registry_id=7596939"
-    },
-    {
-        "name": "TRIP TRAP FEEDING TRAY",
-        "img": "static/gameImages/feedingTray.png",
-        "link": "https://www.babylist.com/gp/stokke-tray/14549/31544?reg_item_id=472022295&registry_id=7596939"
-    },
-    {
-        "name": "BURT'S BEES ONESIES: 5-PACK",
-        "img": "static/gameImages/onesiesFivePack.png",
-        "link": "https://www.babylist.com/gp/burt-s-bees-baby-organic-short-sleeve-bodysuit-5-pack/14427/30958?reg_item_id=473083436&registry_id=7596939"
-    },
-    {
-        "name": "BALLOON ONESIE",
-        "img": "static/gameImages/balloonOnesie.png",
-        "link": "https://milkbarnkids.com/product/vintage-balloons-organic-cotton-short-sleeve-one-piece/"
-    },
-    {
-        "name": "DEER AND FOX LEGGINGS",
-        "img": "static/gameImages/deerFoxLeggings.png",
-        "link": "https://www.winterwaterfactory.com/collections/baby-leggings/products/baby-leggings-deer-foxes-dark-green?variant=42831395848363&sscid=a1k7_gj5n3&"
-    },
-    {
-        "name": "CLASSIC SHORT-SLEEVE ONESIE 3 PACK",
-        "img": "static/gameImages/classicshortOnesieThreePack.png",
-        "link": "https://coloredorganics.com/products/3-pack-classic-short-sleeve-bodysuits?variant=39476811759712"
-    },
-    {
-        "name": "HOODIE AND PANT SET",
-        "img": "static/gameImages/hoodiePantsSet.png",
-        "link": "https://coloredorganics.com/products/organic-cotton-baby-and-kids-aw23-hoodie-and-pant-set?variant=39757673037920"
-    },
-    {
-        "name": "KIMONO GOWN IN HARBOR",
-        "img": "static/gameImages/kimonoGown.png",
-        "link": "https://coloredorganics.com/collections/baby-0-to-24-months/products/organic-cotton-baby-layette-indy-kimono-gown-95005-23?variant=39755874861152"
-    },
-    {
-        "name": "CAR BABY MIRROR",
-        "img": "static/gameImages/mirror.png",
-        "link": "https://www.babylist.com/gp/brica-360-baby-in-sight-pivot-mirror/21458/697850?reg_item_id=472768340&registry_id=7596939"
-    },
-    {
-        "name": "DRAGONS LOVE TACOS",
-        "img": "static/gameImages/dragonsLoveTacos.png",
-        "link": "https://www.babylist.com/gp/ingram-dragons-love-tacos/14115/23214?reg_item_id=473403512&registry_id=7596939"
-    },
-    {
-        "name": "BOWIE MUSLIN ROMPER",
-        "img": "static/gameImages/bubbleRomper.png",
-        "link": "https://coloredorganics.com/collections/organic-baby-rompers/products/organic-cotton-baby-meadow-bowie-muslin-bubble-romper-31200-22?variant=39629088325728"
-    },
-    {
-        "name": "LOVABLE SLEEPER SET",
-        "img": "static/gameImages/lovableSleeperSet.png",
-        "link": "https://coloredorganics.com/collections/organic-baby-sleepers/products/lovable-sleeper-set?variant=39412329381984"
-    },
-    {
-        "name": "CRUZ JOGGER PANTS",
-        "img": "static/gameImages/joggerPants.png",
-        "link": "https://coloredorganics.com/collections/organic-baby-and-kids-joggers-and-sweatpants/products/organic-baby-and-kids-classic-cruz-jogger-85801-18?variant=19877490786400"
-    },
-    {
-        "name": "SENECA APPLES LEGGINGS",
-        "img": "static/gameImages/applesLeggings.png",
-        "link": "https://jamiekay.com/products/organic-cotton-legging-apples-seneca-rock?variant=40370294915153"
-    },
-    {
-        "name": "HATCHLING BUNNY KIMONO",
-        "img": "static/gameImages/bunnyKimono.png",
-        "link": "https://pehr.com/collections/clothing/products/3-piece-set?variant=33713055989848"
-    },
-    {
-        "name": "TURTLE ONESIE",
-        "img": "static/gameImages/turtleOnesie.png",
-        "link": "https://pehr.com/collections/clothing/products/classic-one-piece?variant=40216269389912"
-    },
-    {
-        "name": "ELEPHANT ONESIE",
-        "img": "static/gameImages/elephantOnesie.png",
-        "link": "https://pehr.com/collections/clothing/products/short-sleeve-shoulder-snap-one-piece?variant=39879043219544"
-    },
-    {
-        "name": "MOO, BAA, LA LA LA!",
-        "img": "static/gameImages/mooBaaLaLaLa.png",
-        "link": "https://bookshop.org/p/books/moo-baa-la-la-la-sandra-boynton/10359066?ean=9780671449018&sscid=a1k7_gk76i&utm_source=ShareASale&utm_medium=Affiliate&utm_campaign=389818&utm_term=1535322"
-    },
-    {
-        "name": "PENGUIN TEETHER",
-        "img": "/static/gameImages/penguinTeether.png",
-        "link":"https://weegallery.com/collections/teethers/products/organic-teether-penguin"
-    },
-    {
-        "name": "SHORT SLEEVE ONESIES 5-PACK",
-        "img": "/static/gameImages/shortSleeveOnesiesFivePack.png",
-        "link":"https://www.babylist.com/gp/burt-s-bees-baby-organic-short-sleeve-bodysuit-5-pack/14427/30951?reg_item_id=473083281&registry_id=7596939"
+//Server emits image URLS to client for current and previous items drawn  
+socket.on('items', (items) => {
+    console.log(items[0], items[2])
+    $("#selected").css("background-image", "url(../" + items[0] + ")");
+    $("#registry").css("background-image", "url(../" + items[1] + ")"); 
+})
+
+socket.on('currXes', (xes) => {
+    currXes = xes;
+    console.log(xes);
+})
+
+socket.on('winningTeam', (message) => {
+    alert(message['message']);
+})
+
+socket.on('invalid', (message) => {
+    team = document.getElementById('team-title').innerHTML
+    if (team === message) {
+        alert("Invalid bingo! One or more of your marked items has not been called!")
     }
-
-]
-
-//Requests new bingo item every second
-
+})
 
 //Generates 5x5 square bingo game board
-//idx is an array of integers that correlate to a specific bingo item image to be displayed on each player's board
+//card is an array of randomized bingo item image urls 
 //xes is an array of 0's and 1's indicating if a bingo square has been selected by the player or not
-function displayBoard(idx, xes) {
-    connectUser()
-    //document.getElementById("get-board-btn").style.display = 'none';
+function displayBoard(card, xes) {
+    connectUser();
     const board = document.getElementById("board");
     for (let i = 0; i < 25; i++) {
         const square = document.createElement('div');
@@ -248,7 +44,7 @@ function displayBoard(idx, xes) {
         square.style.fontFamily = 'Gill Sans';
         square.style.fontSize = '130px';
         square.style.padding = 'none';
-        square.style.backgroundImage = 'url(../' + gameItems[idx[i]]["img"] + ')';
+        square.style.backgroundImage = 'url(../' + card[i] + ')';
         square.style.backgroundPosition = 'center';
         square.style.backgroundRepeat = 'no-repeat';
         square.onclick = function() {postX(square)};
@@ -258,6 +54,7 @@ function displayBoard(idx, xes) {
         }
         else{
             square.style.color = 'red';
+            currXes[i] = 1;
         }
         
         board.append(square)
@@ -267,22 +64,99 @@ function displayBoard(idx, xes) {
 
 //marks or unmarks a bingo square with a red x when clicked and sends to flask server to be updated in session data
 function postX(square) {
+    let teamName = document.getElementById('team-title').innerHTML
     if (square.style.color === 'red') {
         square.style.color = 'transparent';
+        currXes[Number(square.id)] = 0;
         $.ajax({
             type: "POST",
             url: "{{ url_for('play') }}",
-            data: {'id': square.id, 'x': 0}
+            data: {'team': teamName, 'id': square.id, 'x': 0}
         })
     } else {
         square.style.color = 'red';
+        currXes[Number(square.id)] = 1;
         $.ajax({
             type: "POST",
             url: "{{ url_for('play') }}",
-            data: {'id': square.id, 'x': 1}
+            data: {'team': teamName, 'id': square.id, 'x': 1}
         })
     }
     return
+}
+
+function checkBingo(team, card) {
+    candidate = bingoChecker(currXes, card);
+    if (candidate) {
+        socket.emit("winner", {'team': team, 'candidate': candidate});
+    }else {
+        alert("You have not checked 5 boxes in a row yet!");
+    }
+    return;   
+}
+
+function bingoChecker(xes, card) {
+    var rows = [0, 5, 10, 15, 20];
+    var cols = [0, 1, 2, 3, 4];
+    for (i = 0; i < 21; i++) {
+        //checks for bingo starting at left diagonal
+        if (i === 0) {
+            var leftDiag = [];
+            for (j = 0; j < 25; j += 6){
+                if (xes[i+j] === 1) {
+                    leftDiag.push(card[i+j]);
+                }else {
+                    break;
+                }
+            }
+            if (leftDiag.length === 5) {
+                return leftDiag;
+            }
+        }
+        //checks for bingo starting at right diagonal
+        if (i === 4) {
+            rightDiag = []
+            for (k = 0; k < 21; k += 4) {
+                if(xes[i+k] === 1) {
+                    rightDiag.push(card[i+j]);
+                }else {
+                    break;
+                }
+            }
+            if (rightDiag.length === 5) {
+                return rightDiag;
+            }
+        }
+        //checks all rows for bingo
+        if (rows.includes(i)) {
+            rowCandidate = [];
+            for (m = 0; m < 5; m++) {
+                if (xes[i+m] === 1) {
+                    rowCandidate.push(card[i+m]);
+                }else {
+                    break;
+                }
+            }
+            if (rowCandidate.length === 5) {
+                return rowCandidate;
+            }
+        }
+        //checks all cols for bingo
+        if (cols.includes(i)) {
+            colCandidate = [];
+            for (n = 0; n < 24; n += 5) {
+                if (xes[i+n] === 1) {
+                    colCandidate.push(card[i+n]);
+                }else {
+                    break;
+                }
+            }
+            if (colCandidate.length === 5) {
+                return colCandidate;
+            }
+        }
+    }
+    return false;
 }
 
 function toggleForm(elem) {
@@ -290,25 +164,10 @@ function toggleForm(elem) {
     return
 }
 
-
-//requests the current item and previous item pulled during the round
-function getItem() {
-    $.get("/game", function(data, elem) {
-        $("#selected").css("background-image", "url(../" + gameItems[data[0]]["img"] + ")");
-        $("#registry").css("background-image", "url(../" + gameItems[data[1]]["img"] + ")");
-    })
-    return
-}
-
-
-
 function connectUser() {
-    const socket = io();
-    socket.on('items', (data) => {
-        $("#selected").css("background-image", "url(../" + data[0] + ")");
-        $("#registry").css("background-image", "url(../" + data[1] + ")");
-        return
-    })
+    socket.connect();
+    var team = document.getElementById('team-title').innerHTML;
+    socket.emit("team", {'team': team});
     return
 }
 
